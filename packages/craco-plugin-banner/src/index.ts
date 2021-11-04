26f90cc97682f6777df7110435da5e8683f76d9f
afeiship <1290657123@qq.com>
2021-11-04 21:41:49 +0800

    feat: update df plugin

--- packages/craco-plugin-banner/src/index.ts
+++ packages/craco-plugin-banner/src/index.ts
@@ -1,10 +1,21 @@
 import HtmlBannerWebpackPlugin from '@jswork/html-banner-webpack-plugin';
+import nx from '@jswork/next';
+import '@jswork/next-absolute-package';
+
+const pkg = nx.absolutePackage();
+const defPluginOptions = {
+  banner: [
+    `NAME: ${pkg.name}`,
+    `VERSION: v${pkg.version}`,
+    `ENV: ${process.env.NODE_ENV}`,
+    'BUILD_TIME: ' + new Date().toLocaleString('en-US', { timeZone: 'Asia/ShangHai' })
+  ]
+};
 
 export = {
   overrideCracoConfig: ({ cracoConfig, pluginOptions, context: { env, paths } }) => {
-    cracoConfig.webpack.plugins.push(
-      new HtmlBannerWebpackPlugin(pluginOptions)
-    );
+    const options = nx.mix(null, defPluginOptions, pluginOptions);
+    cracoConfig.webpack.plugins.push(new HtmlBannerWebpackPlugin(options));
     return cracoConfig;
   }
 };
