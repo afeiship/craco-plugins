d990b364d5800b054ceea942dc5fe00d359368d7
afeiship <1290657123@qq.com>
2021-11-08 10:34:43 +0800

    fix: spa-base merge other config

--- packages/craco-plugin-spa-base/src/index.ts
+++ packages/craco-plugin-spa-base/src/index.ts
@@ -1,10 +1,11 @@
 import { path as dirname } from 'app-root-path';
 import path from 'path';
 import webpack from 'webpack';
+import { merge } from 'webpack-merge';
 
 export = {
   overrideCracoConfig: ({ cracoConfig, pluginOptions, context: { env, paths } }) => {
-    cracoConfig.webpack = {
+    const config = {
       alias: {
         '@': path.join(dirname, 'src')
       },
@@ -16,6 +17,8 @@ export = {
         })
       ]
     };
+
+    merge(cracoConfig.webpack, config);
     return cracoConfig;
   }
 };
